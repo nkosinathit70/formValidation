1e43062af0ebf5f808f3b03cfa537d172aadb684
Codetribe <nkosinathit70@gmail.com>
2018-08-14 14:30:14 +0200

    First commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,13 +1,39 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, NavParams} from 'ionic-angular';
+//import { ErrorHandler, NgModule } from '@angular/core';
+//import {ActionSheetController } from 'ionic-angular';
+//IMPORT FORMbUILDER
+import { FormBuilder, FormGroup, Validators, AbstractControl} from '@angular/forms'
+
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  //DECLARE FORM gROUP 
+  formgroup:FormGroup;
+  //declare your controls
+  firstname:AbstractControl;
+  lastname:AbstractControl;
+  email:AbstractControl;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController,public formBuilder:FormBuilder,public navParams:NavParams) {
+
+  }
+
+  onSubmit(){
+
+    //make validaTIONS OF THE FORM
+    this.formgroup = this.formBuilder.group({
+      firstname:['',Validators.required],
+      lastname:['',Validators.required],
+      email:['',Validators.required]
+    });
+
+    this.firstname = this.formgroup.controls['firstname'];
+    this.lastname = this.formgroup.controls['lastname'];
+    this.email = this.formgroup.controls['email'];
 
   }
 
